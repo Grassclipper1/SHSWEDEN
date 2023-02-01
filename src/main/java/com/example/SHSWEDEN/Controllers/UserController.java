2762881d20dbd4494e0696905a511461a37e5783
MalinJeppsson <malin.jeppsson@brightstraining.com>
2023-02-01 11:19:48 +0100

    Koppling mellan antal objekt på ProfilPage

--- src/main/java/com/example/SHSWEDEN/Controllers/UserController.java
+++ src/main/java/com/example/SHSWEDEN/Controllers/UserController.java
@@ -6,6 +6,7 @@ import com.example.SHSWEDEN.Entities.User;
 import com.example.SHSWEDEN.Repos.CategoryRepository;
 import com.example.SHSWEDEN.Repos.ListingRepository;
 import com.example.SHSWEDEN.Repos.UserRepository;
+import com.example.SHSWEDEN.Services.ListingService;
 import com.example.SHSWEDEN.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -34,6 +35,9 @@ public class UserController {
     ListingRepository listingRepository;
 
     @Autowired
+    ListingService listingService;
+
+    @Autowired
     private UserRepository userRepository;
     @Autowired
     CategoryRepository categoryRepository;
@@ -55,13 +59,17 @@ public class UserController {
     }
 
     @PostMapping("/signin")
-    String login(HttpSession session, @RequestParam String emailAddress, @RequestParam String password) {
+    String login(HttpSession session, Model model, @RequestParam String emailAddress, @RequestParam String password) {
         User user = userService.findByEmailAddressAndPassword(emailAddress, password);
 
         if(user != null && user.getPassword().equals(password)){
             session.setAttribute("userId", user.getId());
             session.setAttribute("seller", user.getId());
             session.setAttribute("user", user);
+
+            List<Listing> listings = listingService.findByUser(user.getId());
+            model.addAttribute("listings", listings);
+
             return "ProfilePage";
         }
         return "signin";
@@ -121,8 +129,13 @@ public class UserController {
    @GetMapping("/ProfilePage")  //använder denna för att ha åtkomst till account för tester
     String ProfilePage(HttpSession session, Model model) {
         Integer id = (Integer) session.getAttribute("userId");
+
         if (id != null) {
+
+            List<Listing> listings = listingService.findByUser(id);
+            model.addAttribute("listings", listings);
             model.addAttribute("seller", id);
+
             return "ProfilePage";
         } else
             session.removeAttribute("userId");
